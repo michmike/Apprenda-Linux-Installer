66724ff503b72b8150829fac796f5a17669a6e27
michmike <michmike@cs.stanford.edu>
2014-02-11 18:45:48 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,9 @@ wget https://raw2.github.com/michmike/Apprenda/master/ci-java-prereqs.sh
 wget https://raw2.github.com/michmike/Apprenda/master/5.0.Latest/argparse.py
 wget https://raw2.github.com/michmike/Apprenda/master/5.0.Latest/hostconfig.py 
 wget https://raw2.github.com/michmike/Apprenda/master/5.0.Latest/prereqChecker.py 
-cd apprenda
-mv argparse.py 5.0.Latest\
-mv prereqChecker.py 5.0.Latest\
-mv hostconfig.py 5.0.Latest\
+mv argparse.py 5.0.Latest/
+mv prereqChecker.py 5.0.Latest/
+mv hostconfig.py 5.0.Latest/
 chmod +x ./ci-functions
 chmod +x ./ci-init-bootstrap.sh
 ./ci-init-bootstrap.sh
